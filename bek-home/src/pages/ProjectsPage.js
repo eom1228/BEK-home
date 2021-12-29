69ee5f1c297e005888833833a184b692ee3c5248
Britty <bek.britty@gmail.com>
2021-12-29 09:26:02 +0545

    change on the name

--- bek-home/src/pages/ProjectsPage.js
+++ bek-home/src/pages/ProjectsPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Gallery from '../components/Gallery/Gallery.';
+import Gallery from '../components/Gallery';
 
 const ProjectsPage = () => {
   return (
