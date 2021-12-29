4f06bd3a0a2be37fdf91ceb6a9d6a5d68f2a28dc
Britty <bek.britty@gmail.com>
2021-12-29 08:58:22 +0545

    start of gallery

--- bek-home/src/pages/ProjectsPage.js
+++ bek-home/src/pages/ProjectsPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Gallery from '../components/Gallery';
+import Gallery from '../components/Gallery/Gallery.';
 
 const ProjectsPage = () => {
   return (
