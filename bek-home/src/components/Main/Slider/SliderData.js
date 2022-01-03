5219625afd076d2274838461b23d7dbcd8682bd1
Britty <bek.britty@gmail.com>
2022-01-03 17:34:57 +0545

    landing page slider

---
+++ bek-home/src/components/Main/Slider/SliderData.js
@@ -0,0 +1,28 @@
+import React from "react";
+import image1 from '../../../images/sample.jpeg';
+import image2 from '../../../images/fnt_sampleImg.jpeg';
+
+export const SliderData= [
+    {
+        label: "First Picture",
+        imgPath:
+            image1,
+    },
+    {
+        label: "Second Picture",
+        imgPath:
+            image2,
+    },
+    {
+        label: "Third Picture",
+        imgPath:
+            image1,
+    },{
+        label: "Second Picture",
+        imgPath:
+            image2,
+    },
+];
+
+
+
--- bek-home/src/pages/LandingPage.js
+++ bek-home/src/pages/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Slider from '../components/Main/Slider/Slider';
 
 const LandingPage = () => {
   return (
     <>
+    <Slider/>
       <div>LandingPage</div>
     </>
   );
