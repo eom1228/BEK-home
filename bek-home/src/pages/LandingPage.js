3e77ce18c46cdc90d6147b29cd934f7fbb5c748d
Britty <bek.britty@gmail.com>
2022-01-04 07:28:38 +0545

    changes in the header for landing page

--- bek-home/src/pages/LandingPage.js
+++ bek-home/src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Slider from '../components/Main/Slider/Slider';
+import '../components/Main/main.scss';
 
 const LandingPage = () => {
   return (
